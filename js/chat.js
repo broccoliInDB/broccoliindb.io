c658613ff828e216f530a1308cc8cc0fa2a08042
broccoli <broccoliindb@gmail.com>
2020-07-16 01:58:25 +0900

    refactoring for index, friends & done chat

---
+++ js/chat.js
@@ -0,0 +1,48 @@
+const container = document.querySelector('.container')
+const add = container.querySelector('.add')
+const main = container.querySelector('.main')
+const chat = main.querySelector('.chat')
+
+main.addEventListener('click', function (evt) {
+  main.classList.remove('show')
+  main.classList.remove('attachment')
+  main.classList.remove('keyboard')
+  Array.from(add.children).forEach(child => {
+    child.classList.remove('show')
+  })
+})
+
+add.addEventListener('click', function (evt) {
+  const target = evt.target
+  if (target.closest('.add__item_attachment')) {
+    addShow(add.children[1])
+    chat.scrollIntoView(false);
+  }
+  if (target.closest('.add__item_keyboard') || target.closest('.add__item_hash')) {
+    addShow(add.children[2])
+    chat.scrollIntoView(false)
+  }
+})
+
+function addShow(target) {
+  if (!main.classList.contains('show')) {
+    main.classList.add('show')
+    if (target.classList.contains('keyboard')) {
+      main.classList.add('keyboard')
+    } else if (target.classList.contains('attachment')) {
+      main.classList.add('attachment')
+    }
+  } else {
+    if (target.classList.contains('keyboard')) {
+      main.classList.remove('attachment')
+      main.classList.add('keyboard')
+    } else if (target.classList.contains('attachment')) {
+      main.classList.remove('keyboard')
+      main.classList.add('attachment')
+    }
+  }
+  Array.from(add.children).forEach(child => {
+    child.classList.remove('show')
+  })
+  target.classList.add('show')
+}
