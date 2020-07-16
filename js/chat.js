cba1cf5fa6cbb057fff2c5d466f5348ff02a5aea
broccoli <broccoliindb@gmail.com>
2020-07-16 15:00:05 +0900

    revised viewer

--- js/chat.js
+++ js/chat.js
@@ -3,7 +3,7 @@ const add = container.querySelector('.add')
 const main = container.querySelector('.main')
 const chat = main.querySelector('.chat')
 
-main.addEventListener('click', function (evt) {
+main.addEventListener('click', function () {
   main.classList.remove('show')
   main.classList.remove('attachment')
   main.classList.remove('keyboard')
@@ -18,9 +18,17 @@ add.addEventListener('click', function (evt) {
     addShow(add.children[1])
     chat.scrollIntoView(false);
   }
-  if (target.closest('.add__item_keyboard') || target.closest('.add__item_hash')) {
+  if (target.closest('.add__item_hash')) {
     addShow(add.children[2])
     chat.scrollIntoView(false)
+  }
+  if (target.closest('.add__item_keyboard')) {
+    main.classList.remove('show')
+    main.classList.remove('keyboard')
+    main.classList.remove('attachment')
+    Array.from(add.children).forEach(child => {
+      child.classList.remove('show')
+    })
   }
 })
 
